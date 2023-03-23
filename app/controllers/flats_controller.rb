94fa984a473c128acb5a6f4d8db6345cd29ca91a
Doug Barr <douglas.barr01@gmail.com>
2023-03-23 10:16:06 +0000

    add set_flat in controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,11 @@
 class FlatsController < ApplicationController
+  before_action :set_flat only: [:show, :edit, :update]
 
   def index
     @flats = Flat.all
   end
 
   def show
-    @flat = Flat.find(params[:id])
   end
 
 
@@ -23,10 +23,27 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
   private
 
   def flat_params
     params.require(:flat).permit(:name, :description, :city, :address, :rooms, :number_of_guests, :price_per_night)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 
 

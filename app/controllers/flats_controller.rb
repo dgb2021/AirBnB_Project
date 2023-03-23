4aa4a92ca1d18059a5227851cd5fc74be2c0efab
Doug Barr <douglas.barr01@gmail.com>
2023-03-23 10:36:39 +0000

    user can delete a flat listing that he has set

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show, :edit, :update]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     @flats = Flat.all
@@ -34,7 +34,10 @@ class FlatsController < ApplicationController
     end
   end
 
-
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
 
   private
 
@@ -45,6 +48,5 @@ class FlatsController < ApplicationController
   def set_flat
     @flat = Flat.find(params[:id])
   end
-
 
 end

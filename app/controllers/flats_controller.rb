a6b51fb48c03c6c811112f4354d7df5459bf50d6
Doug Barr <douglas.barr01@gmail.com>
2023-03-23 10:29:41 +0000

    only show edit & remove if user is the owner of the listing

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat only: [:show, :edit, :update]
+  before_action :set_flat, only: [:show, :edit, :update]
 
   def index
     @flats = Flat.all

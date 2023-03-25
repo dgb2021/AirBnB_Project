45c8764c2c8f39ef8214f6af345e7abd58f6519f
Doug Barr <douglas.barr01@gmail.com>
2023-03-25 14:43:30 +0000

    Adding bookings nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :flats
+  resources :flats, only: [:index, :show, :new, :create, :edit, :destroy] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:destroy]
+  resources :users, only: [:new, :create, :show]
+
 end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
